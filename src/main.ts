efe780288e9fbe9f27bc33e38c8e8306c690bf54
Alonso <codelonso@gmail.com>
2024-09-30 10:53:40 -0500

    Update Standalone App

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+import { bootstrapApplication } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app/app.component';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './routes';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes, withComponentInputBinding())
+  ]
+})
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
