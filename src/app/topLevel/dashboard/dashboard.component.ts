f97ee3366b0935da8ce1b4b89f3c0ed23b55bb34
Alonso <codelonso@gmail.com>
2024-10-01 12:40:39 -0500

    Content with sidebar

--- src/app/topLevel/dashboard/dashboard.component.ts
+++ src/app/topLevel/dashboard/dashboard.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from "../../components/sidebar/sidebar.component";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     RouterModule,
+    NgClass,
     SidebarComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-
+  public sidebarExpanded:boolean
 }
