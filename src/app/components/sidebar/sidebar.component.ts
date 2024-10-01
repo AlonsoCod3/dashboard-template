78d3707fa963aeeb1fb2f9472a1fdcdb111b7e2e
Alonso <codelonso@gmail.com>
2024-10-01 11:57:29 -0500

    Sidebar component funcionality

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,32 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    RouterModule,
+    NgClass,
+    NgFor,
+    NgIf
+  ],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  public isOpen:boolean = true
+  public image:boolean = false
 
+  public NAME:string = "Dashboard Template"
+
+  // url - icon - nombre - sub
+  public routes = [
+    [ "", "not" ,"Inicio"  ],
+    [ "menu-1", "not" ,"Menu lateral 1", [
+      [ "sub-1", "not" ,"SubMenu 1"  ],
+      [ "sub-2", "not" ,"SubMenu 2"  ],
+    ]
+    ],
+  ]
 }
