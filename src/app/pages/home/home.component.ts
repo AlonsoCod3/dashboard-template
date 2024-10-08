8656101043d56d5f0c330c883552b91e6f135052
Alonso <codelonso@gmail.com>
2024-10-07 23:10:45 -0500

    Default Home Page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,20 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgFor],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  title
+
+  constructor() {
+    this.title = "Bienvenido Usuari@"
+    
+  }
+  
+  ngOnInit(): void {}
 
 }
