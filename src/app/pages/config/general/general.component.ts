6c0faa98d7230e8a5500006a63f83bc33803fa70
Alonso <codelonso@gmail.com>
2024-10-22 22:25:44 -0500

    Send data to show

--- src/app/pages/config/general/general.component.ts
+++ src/app/pages/config/general/general.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Output, EventEmitter } from '@angular/core';
 import { JsonPipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { HomeComponent } from '../../home/home.component';
 import { HttpClient } from '@angular/common/http';
@@ -14,14 +14,13 @@ import { TableComponent } from "../../../components/table/table.component";
   styleUrls: ['./general.component.css',]
 })
 export class GeneralComponent implements OnInit{
-  // @ViewChild("selector") col:ElementRef
   title:string = "Configuración general"
   titlePage:string = "Configuración"
 
   table:boolean
   colorpicker = ["#1098F7","#B89E97","#DECCCC"]
   pickColor = "#aa1f2b"
-  values:Object
+  @Output() values =new EventEmitter<any>()
   url = "https://my.api.mockaroo.com/clientes?key=f23ee800"
   options:Object // opciones switch para activar
 
@@ -53,7 +52,7 @@ export class GeneralComponent implements OnInit{
     this.http.get(this.url).subscribe(
       data => {
         // console.log(data)
-        this.values = data
+        this.values.emit(data)
       }
     )
   }
